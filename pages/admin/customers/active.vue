9b4a5a89ace7ee79d4b96ed4b2acc0d9af5147d8
felipevegners <felipevegners@gmail.com>
2025-01-17 09:50:11 -0300

    build: customer form and db data

--- pages/admin/customers/active.vue
+++ pages/admin/customers/active.vue
@@ -8,7 +8,7 @@ useHead({
   title: "Clientes ativos"
 });
 
-import { Plus } from "lucide-vue-next";
+import { Plus, LoaderCircle } from "lucide-vue-next";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { createColumnHelper } from "@tanstack/vue-table";
@@ -16,11 +16,19 @@ import { ArrowUpDown } from "lucide-vue-next";
 import { h } from "vue";
 import DropdownAction from "@/components/shared/DataTableDropDown.vue";
 import DataTable from "~/components/shared/DataTable.vue";
+import FormSelect from "~/components/shared/FormSelect.vue";
 
 const customers = ref<any>([]);
+const isLoading = ref<boolean>(true);
 onMounted(async () => {
-  const data = await $fetch("/api/admin/customers");
-  customers.value = data;
+  try {
+    isLoading.value = true;
+    const data = await $fetch("/api/admin/customers");
+    customers.value = data;
+  } catch (error) {
+  } finally {
+    isLoading.value = false;
+  }
 });
 
 export interface Payment {
@@ -88,10 +96,6 @@ const columns = [
   }),
   columnHelper.accessor("name", {
     enablePinning: true,
-    header: "Nome",
-    cell: ({ row }) => h("div", { class: "capitalize" }, row.getValue("name"))
-  }),
-  columnHelper.accessor("document", {
     header: ({ column }) => {
       return h(
         Button,
@@ -99,22 +103,26 @@ const columns = [
           variant: "ghost",
           onClick: () => column.toggleSorting(column.getIsSorted() === "asc")
         },
-        () => ["CNPJ", h(ArrowUpDown, { class: "ml-2 h-4 w-4" })]
+        () => ["Nome", h(ArrowUpDown, { class: "ml-2 h-4 w-4" })]
       );
     },
+    cell: ({ row }) => h("div", { class: "capitalize" }, row.getValue("name"))
+  }),
+  columnHelper.accessor("document", {
+    header: () => h("div", { class: "text-left" }, "CNPJ"),
     cell: ({ row }) =>
       h("div", { class: "lowercase" }, row.getValue("document"))
   }),
   columnHelper.accessor("phone", {
-    header: () => h("div", { class: "text-right" }, "Contato"),
+    header: () => h("div", { class: "text-left" }, "Contato"),
     cell: ({ row }) => h("div", { class: "capitalize" }, row.getValue("phone"))
   }),
   columnHelper.accessor("website", {
-    header: () => h("div", { class: "text-right" }, "Site"),
+    header: () => h("div", { class: "text-left" }, "Site"),
     cell: ({ row }) => {
       return h(
         "div",
-        { class: "text-right font-medium" },
+        { class: "text-left font-medium" },
         row.getValue("website")
       );
     }
@@ -122,13 +130,13 @@ const columns = [
   columnHelper.display({
     id: "actions",
     enableHiding: false,
-    header: () => h("div", { class: "text-right" }, "Ações"),
+    header: () => h("div", { class: "text-left" }, "Ações"),
     cell: ({ row }) => {
       const payment = row.original;
 
       return h(
         "div",
-        { class: "relative text-right" },
+        { class: "relative text-left" },
         h(DropdownAction, {
           payment,
           onExpand: row.toggleExpanded
@@ -148,8 +156,8 @@ const toggleShowAddForm = () => {
 <template>
   <main class="p-6">
     <section class="mb-6 flex items-center gap-6">
-      <h1 class="font-bold text-black text-2xl">Clientes ativos</h1>
-      <div>
+      <h1 class="font-bold text-black text-3xl">Clientes ativos</h1>
+      <div v-if="!showAddForm">
         <Button @click="toggleShowAddForm">
           <Plus class="w-4 h-4" /> Cadastrar cliente
         </Button>
@@ -162,13 +170,111 @@ const toggleShowAddForm = () => {
         </CardHeader>
         <CardContent>
           <form action="">
-            <Label>Nome da Empresa</Label>
-            <Input type="text" />
+            <FormField name="name" class="mb-4 w-full grid grid-cols-2 gap-8">
+              <FormItem>
+                <FormLabel>Nome da Empresa</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Insira o nome da empresa" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+              <FormItem>
+                <FormLabel>CNPJ</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="XX.XXX.XXX/0001-XX" />
+                </FormControl>
+                <!-- <FormDescription class="text-sm text-red-500">
+                  This is your public display name.
+                </FormDescription> -->
+                <FormMessage />
+              </FormItem>
+            </FormField>
+            <FormField name="name" class="mb-4 grid grid-cols-4 gap-8">
+              <FormItem class="col-span-2">
+                <FormLabel>Endereço</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Insira o endereço" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+              <FormItem class="col-span-1">
+                <FormLabel>Número</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Insira o número" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+              <FormItem>
+                <FormLabel>CEP</FormLabel>
+                <FormControl>
+                  <Input type="number" placeholder="Insira o número" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            </FormField>
+            <FormField name="name" class="grid grid-cols-3 gap-8">
+              <FormItem>
+                <FormLabel>Telefone</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="(XX) XXXXX-XXXX" />
+                </FormControl>
+              </FormItem>
+              <FormItem>
+                <FormLabel>Site</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="www.site.com.br" />
+                </FormControl>
+              </FormItem>
+              <FormItem>
+                <FormLabel>Logo Empresa</FormLabel>
+                <FormControl>
+                  <Input type="file" placeholder="Insira o logo da empresa" />
+                </FormControl>
+              </FormItem>
+            </FormField>
+            <div class="my-10">
+              <h2 class="mb-4 text-lg font-bold">Gerente da Conta</h2>
+              <Separator class="mb-4" />
+              <FormField name="name" class="grid grid-cols-3 gap-8">
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl> </FormControl>
+                </FormItem>
+                <FormItem>
+                  <FormLabel>Telefone</FormLabel>
+                  <FormControl>
+                    <Input type="text" placeholder="(XX) XXXXX-XXXX" />
+                  </FormControl>
+                </FormItem>
+                <FormItem>
+                  <FormLabel>E-mail de Contato</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="nome@email.com.br" />
+                  </FormControl>
+                </FormItem>
+              </FormField>
+              <FormSelect />
+            </div>
+
+            <Button type="submit"> Cadastrar </Button>
+            <Button
+              variant="ghost"
+              class="ml-4"
+              @click.prevent="toggleShowAddForm"
+            >
+              Cancelar
+            </Button>
           </form>
         </CardContent>
       </Card>
     </section>
-    <section>
+    <section
+      v-if="isLoading"
+      class="p-10 h-full flex items-center justify-center"
+    >
+      <LoaderCircle class="w-10 h-10 animate-spin" />
+    </section>
+    <section v-else>
       <DataTable :columns="columns" :data="customers" sortby="name" />
     </section>
   </main>
