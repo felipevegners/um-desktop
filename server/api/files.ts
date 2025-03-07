3dd73f6343c40eb861008efe00cad1668f0ec134
Felipe Vegners <fe_assunpcao@luizalabs.com>
2025-03-07 14:54:38 -0300

    chore: remove files folder

--- server/api/files.ts
+++ server/api/files.ts
@@ -4,7 +4,7 @@ export default defineEventHandler(async (event) => {
     const { files } = await readBody<{ files: ServerFile[] }>(event);
     const fileNames: string[] = [];
     for (const file of files) {
-        fileNames.push(await storeFileLocally(file, 12, ''));
+        fileNames.push(await storeFileLocally(file, 12, '/files'));
     }
     return fileNames;
 });
