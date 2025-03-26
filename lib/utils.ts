b3323eeddd797892d91ec926ca24b24f1e10a68a
felipevegners <felipevegners@gmail.com>
2025-03-26 10:19:58 -0300

    fix: add lib utils helpers

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,15 +1,34 @@
-import type { Updater } from '@tanstack/vue-table'
-import type { Ref } from 'vue'
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import type { Updater } from '@tanstack/vue-table';
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import type { Ref } from 'vue';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export function valueUpdater<T extends Updater<any>>(updaterOrValue: T, ref: Ref) {
-  ref.value
-    = typeof updaterOrValue === 'function'
+export function valueUpdater<T extends Updater<any>>(
+  updaterOrValue: T,
+  ref: Ref,
+) {
+  ref.value =
+    typeof updaterOrValue === 'function'
       ? updaterOrValue(ref.value)
-      : updaterOrValue
+      : updaterOrValue;
 }
+
+export const getDate = () => {
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+  const date = today.getDate();
+  return `${date}/${month}/${year}`;
+};
+
+export const dateFormat = (date: any) => {
+  return new Date(date).toLocaleDateString('pt-BR', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+};
