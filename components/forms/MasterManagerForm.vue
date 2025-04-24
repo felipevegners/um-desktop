d8c2f80f024975e39041ff60a6cdcafd463c40f4
felipevegners <felipevegners@gmail.com>
2025-04-24 12:27:30 -0300

    fix: manager form edit mode conditions

--- components/forms/MasterManagerForm.vue
+++ components/forms/MasterManagerForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Lock, User, WandSparkles } from 'lucide-vue-next';
+import { Info, Lock, User, WandSparkles } from 'lucide-vue-next';
 import { vMaska } from 'maska/vue';
 import { storeToRefs } from 'pinia';
 import { useContractsStore } from '~/stores/admin/contracts.store';
@@ -57,61 +57,58 @@ defineProps<{
       </FormField>
       <div class="col-span-4 p-6 border border-zinc-900 rounded-md">
         <p class="font-bold">Dados de Acesso</p>
-        <p v-if="!editMode" class="text-muted-foreground text-sm">
-          O Gestor Master usará os dados abaixo para acessar a plataforma
-        </p>
-        <p v-else class="text-muted-foreground text-sm">
-          Para editar os dados de acesso (Nome, Email e Senha) do Gestor Master
-          clique no botão abaixo
-        </p>
-        <Button
-          type="button"
-          class="my-4"
-          @click="
-            navigateTo(
-              {
-                name: 'admin-accounts-edit-id',
-                params: { id: editId },
-              },
-              { open: { target: '_blank' } },
-            )
-          "
-        >
-          <User />
-          Editar Conta de Usuário
-        </Button>
-        <div v-if="!editMode" class="mt-6 grid grid-cols-3 gap-6 items-end">
-          <FormField v-slot="{ componentField }" name="managerEmail">
-            <FormItem class="relative">
-              <FormLabel>E-mail de Acesso</FormLabel>
-              <FormControl>
-                <Input type="email" v-bind="componentField" />
-              </FormControl>
-              <FormMessage
-                class="p-2 absolute w-full bg-red-500 text-white text-sm rounded-md"
-              />
-            </FormItem>
-          </FormField>
-          <FormField v-slot="{ componentField }" name="password">
-            <FormItem>
-              <FormLabel>Senha Temporária</FormLabel>
-              <FormControl>
-                <Input type="text" v-bind="componentField" maxlength="8" />
-              </FormControl>
-            </FormItem>
-          </FormField>
+        <div v-if="editMode">
+          <p class="text-muted-foreground text-sm">
+            Para editar os dados de acesso (Nome, Email e Senha) do Gestor
+            Master clique no botão abaixo
+          </p>
           <Button
-            v-if="editMode"
-            class="mb-1 px-2 max-w-[190px]"
-            @click.prevent=""
+            type="button"
+            class="my-4"
+            @click="
+              navigateTo(
+                {
+                  name: 'admin-accounts-edit-id',
+                  params: { id: editId },
+                },
+                { open: { target: '_blank' } },
+              )
+            "
           >
-            <Lock class="w-6 h-6" />
-            Gerar Nova Senha
+            <User />
+            Editar Conta de Usuário
           </Button>
-          <Button v-else class="mb-1 px-2 max-w-[140px]" @click.prevent="">
-            <WandSparkles class="w-6 h-6" />
-            Gerar Senha
-          </Button>
+        </div>
+        <div v-else>
+          <p class="flex items-center gap-1 text-muted-foreground text-sm">
+            <Info :size="14" />
+            O Gestor Master usará os dados abaixo para acessar a plataforma
+          </p>
+          <div class="mt-6 grid grid-cols-3 gap-6 items-end">
+            <FormField v-slot="{ componentField }" name="managerEmail">
+              <FormItem class="relative">
+                <FormLabel>E-mail de Acesso</FormLabel>
+                <FormControl>
+                  <Input type="email" v-bind="componentField" />
+                </FormControl>
+                <FormMessage
+                  class="p-2 absolute w-full bg-red-500 text-white text-sm rounded-md"
+                />
+              </FormItem>
+            </FormField>
+            <FormField v-slot="{ componentField }" name="password">
+              <FormItem>
+                <FormLabel>Senha Temporária</FormLabel>
+                <FormControl>
+                  <Input type="text" v-bind="componentField" maxlength="8" />
+                </FormControl>
+              </FormItem>
+            </FormField>
+            <Button class="mb-1 px-2 max-w-[140px]" @click.prevent="">
+              <WandSparkles class="w-6 h-6" />
+              Gerar Senha
+            </Button>
+          </div>
         </div>
       </div>
     </div>
